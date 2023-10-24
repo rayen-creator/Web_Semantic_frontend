22b88aa822524bab10c1f2d1c763b4e65d24c53e
rayen-creator <rayenoueslati60@gmail.com>
2023-10-24 22:19:06 +0100

    🎉 initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:AuthComponent},
+  { path: 'module', loadChildren: () => import('./components/module/module.module').then(m => m.ModuleModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/module/module.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModuleRoutingModule } from './module-routing.module';
+import { ModuleComponent } from './module.component';
+import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+
+
+@NgModule({
+  declarations: [
+    ModuleComponent,
+    AboutComponent,
+    ProfileComponent,
+    CompaniesComponent,
+    JobsComponent,
+    ProjectsComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [
+    CommonModule,
+    ModuleRoutingModule
+  ]
+})
+export class ModuleModule { }
