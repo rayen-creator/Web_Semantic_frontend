7079539c48dded4d8b2ea7126aa8e707be2e07e1
ghaith <ghaith.benrzig@esprit.tn>
2023-10-31 19:36:40 +0100

    announcements implement service

---
+++ src/app/core/services/Announcement.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnnouncementService {
+  url = 'http://localhost:8005/SpringMVC/controller';
+
+  constructor(private httpclient: HttpClient) { }
+
+  getAnnouncements() {
+    return this.httpclient.get(this.url + '/getAnnouncements');
+  }
+
+  getAnnouncementById(id: string) {
+    return this.httpclient.get(`${this.url}/getAnnouncementById/${id}`);
+  }
+}
