4b5849d2f38581becece8e622f3b6bd8326a17cb
rayen-creator <rayenoueslati60@gmail.com>
2023-11-01 20:21:03 +0100

    🐛 fixed routes

--- src/app/components/module/events/events-routing.module.ts
+++ src/app/components/module/events/events-routing.module.ts
@@ -4,18 +4,15 @@ import { EventsComponent } from './events.component';
 import { ListEventsComponent } from './list-events/list-events.component';
 import { EventDetailsComponent } from './event-details/event-details.component';
 
-
-
-const routes: Routes = [{ path: '', component: EventsComponent },
-{ path: 'list', component: ListEventsComponent },
-{ path: 'details/:label', component: EventDetailsComponent },
-{ path: 'list/:Category_event', component:  ListEventsComponent },
-
-
+const routes: Routes = [
+  { path: '', component: EventsComponent },
+  { path: 'list', component: ListEventsComponent },
+  { path: 'details/:label', component: EventDetailsComponent },
+  { path: 'list/:Category_event', component: ListEventsComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class EventsRoutingModule { }
+export class EventsRoutingModule {}
