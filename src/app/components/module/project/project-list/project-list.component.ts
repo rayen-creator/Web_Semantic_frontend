e46ed9814e259bdd603ca3d36c3d54415925fa99
EmnaTaalouch <emna.taalouch@esprit.tn>
2023-11-01 15:50:32 +0100

    small changes

--- src/app/components/module/project/project-list/project-list.component.ts
+++ src/app/components/module/project/project-list/project-list.component.ts
@@ -8,6 +8,9 @@ import { ProjectService } from 'src/app/core/services/project.service';
 })
 export class ProjectListComponent implements OnInit {
   projects: any[] = [];
+  filteredProjects: any[] = [];
+  nameFilter: string = '';
+  statusFilter: string = '';
 
   constructor(private projectService: ProjectService) {}
 
@@ -20,5 +23,56 @@ export class ProjectListComponent implements OnInit {
       });
       console.log('Fetched projects:', this.projects);
     });
+  }
+  fetchProjects() {
+    this.projectService.getProjects().subscribe((data: any) => {
+      this.projects = data.map((item: any) => {
+        const key = Object.keys(item)[0];
+        return { ...item[key], id: key };
+      });
+    });
+  }
+
+  // Example: Filter projects by name and status
+  filterProjectsByNameAndStatus(
+    Pro_Name: string,
+    status: string,
+    action: string
+  ) {
+    if (action == 'proname') {
+      if (Pro_Name)
+        this.projectService
+          .getProjectByProName(Pro_Name)
+          .subscribe((data: any) => {
+            this.projects = data.map((item: any) => {
+              const key = Object.keys(item)[0];
+              return { ...item[key], id: key };
+            });
+          });
+      else this.fetchProjects();
+    } else if (action == 'status') {
+      if (status)
+        this.projectService
+          .getProjectsByStatus(status)
+          .subscribe((data: any) => {
+            this.projects = data.map((item: any) => {
+              const key = Object.keys(item)[0];
+              return { ...item[key], id: key };
+            });
+          });
+      else this.fetchProjects();
+    } else {
+      // Reset to all projects if no filters are applied
+      this.fetchProjects();
+    }
+  }
+
+  // Add the applyFilters method to the component
+  applyFilters(action: string) {
+    this.filterProjectsByNameAndStatus(
+      this.nameFilter,
+      this.statusFilter,
+      action
+    );
   }
 }
--- src/app/components/module/project/project.module.ts
+++ src/app/components/module/project/project.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectComponent } from './project.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 
 @NgModule({
-  declarations: [ProjectComponent, ProjectListComponent, ProjectDetailComponent],
-  imports: [CommonModule, ProjectRoutingModule],
+  declarations: [
+    ProjectComponent,
+    ProjectListComponent,
+    ProjectDetailComponent,
+  ],
+  imports: [CommonModule, ProjectRoutingModule, FormsModule],
 })
 export class ProjectModule {}
